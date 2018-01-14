8a1cbe61c05a63542404d127e4c770f8c03f32f3
Shuopeng WANG <wang.shuopeng1993@gmail.com>
2018-01-14 11:03:58 +0100

    Add files via upload

---
+++ NominalAttributeClassObserver.py
@@ -0,0 +1,7 @@
+from skmultiflow.classification.core.attribute_class_observers.nominal_attribute_class_observer \
+    import NominalAttributeClassObserver
+
+
+class NominalAttributeClassObserver(NominalAttributeClassObserver):
+    def observe_attribute_class(self, att_val, class_val, weight):
+        pass
---
+++ my_classifier.py
@@ -0,0 +1,30 @@
+from numpy import *
+import numpy as np
+from skmultiflow.classification.trees.hoeffding_tree import HoeffdingTree
+from MutltiLabelInfoGainSplitCriterion import MutltiLabelInfoGainSplitCriterion
+from NominalAttributeClassObserver import NominalAttributeClassObserver
+
+
+class MultiLabelHoeffdingTree(HoeffdingTree):
+
+    class ActiveLearningNode(HoeffdingTree.ActiveLearningNode):
+        def learn_from_instance(self, X, y, weight, ht):
+            pass
+
+    def partial_fit(self, X, y=None, classes=None):
+        # TODO
+        # if not initialized ...
+            # Setup
+        # N.B.: The 'classes' option is not important for this classifier
+        # HINT: You can build a decision tree model on a set of data like this:
+        #       h = DecisionTreeClassifier()
+        #       h.fit(X_batch,y_batch)
+        #       self.H.append(h) # <-- and append it to the ensemble
+
+        return self
+
+    def predict(self, X):
+        # TODO
+        N, D = X.shape
+        # You also need to change this line to return your prediction instead of 0s:
+        return zeros(N)
