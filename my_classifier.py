4510b8e1733811da6ce376bf1329fbb110f6cf30
Shuopeng WANG <wang.shuopeng1993@gmail.com>
2018-01-18 18:07:53 +0100

    Update my_classifier.py

--- my_classifier.py
+++ my_classifier.py
@@ -6,25 +6,88 @@ from NominalAttributeClassObserver import NominalAttributeClassObserver
 
 
 class MultiLabelHoeffdingTree(HoeffdingTree):
+    class Majority():
+        def __init__():
+            self.vectorCounts = {}
+            self.majority_labelset = None
+            self.max_value = 0
 
-    class ActiveLearningNode(HoeffdingTree.ActiveLearningNode):
+        def setLearningImpl(self):
+            self.majority_labelset = None
+            return self
+
+        def partial_fit(self, X, y, weight):
+            L = y.shape()[0]
+            labels = str(y)
+            if(labels in self.vectorCounts):
+                weight += self.vectorCounts[labels]
+            self.vectorCounts[labels] = weight
+            if (weight > self.max_value):
+                self.max_value = weight
+                self.majority_labelset = labels
+
+        def get_votes_for_instance(self, X):
+            return self.vectorCounts
+
+    class MultilabelLearningNodeClassifieringNode(HoeffdingTree.ActiveLearningNode):
+        def __init__(self, initial_class_observations, classification=None, HoeffdingTreeClassifLeaves ht):
+            super().__init__(initial_class_observations)
+            if classification == None:
+                self.classification = Majority()
+                self.classification.setLearningImpl()
+            else:
+                self.classification = classification
+
         def learn_from_instance(self, X, y, weight, ht):
-            pass
+            self.classification.partial_fit(X, y, weight)
+            if not self._is_initialized:
+                self._attribute_observers = [None] * len(X)
+                self._is_initialized = True
+            for l in y:
+                if l != 0 and l != '0':
+                    if l not in self._observed_class_distribution:
+                        self._observed_class_distribution[l] = weight
+                    else:
+                        self._observed_class_distribution[l] += weight
+            for i in range(len(X)):
+                obs = self._attribute_observers[i]
+                if obs is None:
+                    if i in ht.nominal_attributes:
+                        obs = NominalAttributeClassObserver()
+                    else:
+                        obs = GaussianNumericAttributeClassObserver()
+                    self._attribute_observers[i] = obs
+                for l in y:
+                    if l != 0 and l != '0':
+                        obs.observe_attribute_class(X[i], int(l), weight)
+            return self
 
-    def partial_fit(self, X, y=None, classes=None):
-        # TODO
-        # if not initialized ...
-            # Setup
-        # N.B.: The 'classes' option is not important for this classifier
-        # HINT: You can build a decision tree model on a set of data like this:
-        #       h = DecisionTreeClassifier()
-        #       h.fit(X_batch,y_batch)
-        #       self.H.append(h) # <-- and append it to the ensemble
+        def get_class_votes(self, X, ht):
+            return self.classification.get_votes_for_instance(X)
 
-        return self
+#    def partial_fit(self, X, y=None, classes=None):
+#        super.partial_fit(X, y, classes)
+#        return self
+
+    def _new_learning_node(self, initial_class_observations=None):
+        if initial_class_observations is None:
+            initial_class_observations = {}
+        return self.MultilabelLearningNodeClassifieringNode(initial_class_observations)
 
     def predict(self, X):
         # TODO
-        N, D = X.shape
-        # You also need to change this line to return your prediction instead of 0s:
-        return zeros(N)
+        r, _ = get_dimensions(X)
+        predictions = []
+        for i in range(r):
+            votes = self.get_votes_for_instance(X[i])
+            if votes == {}:
+                # Tree is empty, all classes equal, default to zero
+                predictions.append(0)
+            else:
+                predictions.append(max(votes, key=votes.get)
+                                   )
+        return predictions
+
+
+#multilabelHT = MultiLabelHoeffdingTree()
+
