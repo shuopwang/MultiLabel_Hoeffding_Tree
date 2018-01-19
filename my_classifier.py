61d2f6b8d5804f11722cb082fe27a7bb5ecce840
Shuopeng WANG <wang.shuopeng1993@gmail.com>
2018-01-19 10:34:55 +0100

    Update my_classifier.py

--- my_classifier.py
+++ my_classifier.py
@@ -1,32 +1,36 @@
 from numpy import *
 import numpy as np
 from skmultiflow.classification.trees.hoeffding_tree import HoeffdingTree
+from skmultiflow.classification.core.attribute_class_observers.gaussian_numeric_attribute_class_observer import GaussianNumericAttributeClassObserver
+from skmultiflow.classification.core.attribute_class_observers.nominal_attribute_class_observer import NominalAttributeClassObserver
+from skmultiflow.classification.core.attribute_class_observers.null_attribute_class_observer import NullAttributeClassObserver
+
+
+class Majority():
+    def __init__(self):
+        self.vectorCounts = {}
+        self.majority_labelset = None
+        self.max_value = 0
+
+    def setLearningImpl(self):
+        self.majority_labelset = None
+        return self
+
+    def partial_fit(self, X, y, weight):
+
+        labels = str(y)
+        if(labels in self.vectorCounts):
+            weight += self.vectorCounts[labels]
+        self.vectorCounts[labels] = weight
+        if (weight > self.max_value):
+            self.max_value = weight
+            self.majority_labelset = labels
+
+    def get_votes_for_instance(self, X):
+        return self.vectorCounts
 
 
 class MultiLabelHoeffdingTree(HoeffdingTree):
-    class Majority():
-        def __init__():
-            self.vectorCounts = {}
-            self.majority_labelset = None
-            self.max_value = 0
-
-        def setLearningImpl(self):
-            self.majority_labelset = None
-            return self
-
-        def partial_fit(self, X, y, weight):
-            L = y.shape()[0]
-            labels = str(y)
-            if(labels in self.vectorCounts):
-                weight += self.vectorCounts[labels]
-            self.vectorCounts[labels] = weight
-            if (weight > self.max_value):
-                self.max_value = weight
-                self.majority_labelset = labels
-
-        def get_votes_for_instance(self, X):
-            return self.vectorCounts
-
     class MultilabelLearningNodeClassifieringNode(HoeffdingTree.ActiveLearningNode):
         def __init__(self, initial_class_observations, classification=None):
             super().__init__(initial_class_observations)
@@ -41,8 +45,13 @@ class MultiLabelHoeffdingTree(HoeffdingTree):
             if not self._is_initialized:
                 self._attribute_observers = [None] * len(X)
                 self._is_initialized = True
+            y = np.asarray(y)
+            #print(y)
+            #print(y.shape)
+            y = y.reshape((1, 1))
             for l in y:
                 if l != 0 and l != '0':
+                    l = int(l)
                     if l not in self._observed_class_distribution:
                         self._observed_class_distribution[l] = weight
                     else:
