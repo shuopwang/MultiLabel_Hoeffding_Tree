107925c5060acdbb7b646fcf61e92b040082b83c
Shuopeng WANG <wang.shuopeng1993@gmail.com>
2018-02-10 23:36:36 +0100

    Update run_experiments.py

--- run_experiments.py
+++ run_experiments.py
@@ -7,22 +7,27 @@ from sklearn.linear_model.stochastic_gradient import SGDClassifier
 from skmultiflow.options.file_option import FileOption
 from skmultiflow.data.file_stream import FileStream
 from my_classifier import MultiLabelHoeffdingTree
-from multi_label import BR
-from multi_label import LC
-dataset = "elec"
+from BR_hoeffding import BR_Hoeffding
+dataset = "Naive_imdb"
 
 # 1. Create a stream
-opt = FileOption("FILE", "OPT_NAME", "./data/"+dataset+".csv", "CSV", False)
-stream = FileStream(opt, -1, 1)
+opt = FileOption("FILE", "OPT_NAME", "" + dataset + ".csv", "CSV", False)
+stream = FileStream(opt, 1, 27)
 # 2. Prepare for use
 stream.prepare_for_use()
+print("The number of targets: ", stream.get_info())
+
 # 2. Instantiate the HoeffdingTree classifier
-h = [	BR(h=SGDClassifier(n_iter=1)),
-        #KNN(k=10, max_window_size=100, leaf_size=30), 
-        #HoeffdingTree(),
-        MultiLabelHoeffdingTree(),
-     ]
+# h = [	#BR(h=SGDClassifier(n_iter=1)),
+#KNN(k=10, max_window_size=100, leaf_size=30),
+# HoeffdingTree(),
+# MLkNN(max_window_size=100),
+#       MultiLabelHoeffdingTree(),
+#   ]
+
+h = BR_Hoeffding()
 # 3. Setup the evaluator
-eval = EvaluatePrequential(pretrain_size=1000, output_file='result_'+dataset+'.csv', max_instances=10000, batch_size=1, n_wait=500, max_time=1000000000, task_type='classification', show_plot=False, plot_options=['performance'])
+eval = EvaluatePrequential(pretrain_size=1000, output_file='result_' + dataset + '.csv', max_instances=10000, batch_size=1,
+                           n_wait=500, max_time=1000000000, task_type='multi_output', show_plot=False, plot_options=['performance'])
 # 4. Run
 eval.eval(stream=stream, classifier=h)
