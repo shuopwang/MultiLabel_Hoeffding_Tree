01ca284e6bbdae2cb2fa6df4bd0d723b9a9ad81b
Shuopeng WANG <wang.shuopeng1993@gmail.com>
2018-02-11 13:21:18 +0100

    Update BR_hoeffding.py

--- classification/BR_hoeffding.py
+++ classification/BR_hoeffding.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
-from skmultiflow.classification.trees.hoeffding_tree import HoeffdingTree
+from hoeffding_tree import HoeffdingTree
 
 
 class BR_Hoeffding:
@@ -19,6 +19,10 @@ class BR_Hoeffding:
         return self
 
     def partial_fit(self, X, y, classes=None):
+        # print("---" * 20 + "debug partial_fit BEGINNING......" + "---" * 20)
+        # print("The shape of X: ", len(X), "  ", len(X[0]))
+        # print("The shape of y: ", len(y), "  ", len(y[0]))
+        # print("---" * 20 + "debug partial_fit ENDING........." + "---" * 20)
         if len(self.hoeffdingTreesList) == 0:
             self.hoeffdingTreesList = []
             for i in range(len(y[0])):
@@ -31,8 +35,7 @@ class BR_Hoeffding:
 
     def predict(self, X):
         results = []
-
-        #print("The shape of X: ", len(X), "  ", len(X[0]))
+        # print("The shape of X: ", len(X), "  ", len(X[0]))
         if len(self.hoeffdingTreesList) == 0:
             results = np.zeros((len(X), self.L))
             print("-" * 10 + "debug 1" + "-" * 10)
@@ -42,12 +45,17 @@ class BR_Hoeffding:
         c = None
         tmp = []
         for index in range(len(X)):
+            #print("-" * 15 + "debug predict BEGINNING" + "-" * 15)
+            #print("The shape of X: ", len(X), "  ", len(X[index]))
+            #print("-" * 15 + "debug predict ENDING" + "-" * 15)
             for i in range(len(self.hoeffdingTreesList)):
                 c = self.hoeffdingTreesList[i].predict(X[index])
                 tmp.append(c[0])
             results.append(tmp)
             tmp = []
-        #print("-" * 10 + "debug 2" + "-" * 10)
-        #print("results:", results)
-        #print("-" * 10 + "debug 2" + "-" * 10)
+#        print("-" * 20 + "debug 2" + "-" * 20)
+#        print("type of results:", type(results))
+#        print("results: ", results)
+#        print("-" * 20 + "debug 2" + "-" * 20)
+
         return results
